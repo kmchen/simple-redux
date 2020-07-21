3ec1784df74b628a8f4dc5e6783f6ef5438d821d
Kuang-Ming Chen <chen.kuangming@kreditech.com>
2020-07-21 13:13:55 +0200

    Add saga runner

--- src/index.js
+++ src/index.js
@@ -3,6 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+
+function runner(genFunc) {
+  const iterator = genFunc();
+  function next(args) {
+    console.log(args && args.data)
+    let resp = iterator.next(args); 
+    if(resp.done) {
+      return args;
+    } else {
+      return Promise.resolve(resp.value).then(next) 
+    }
+  }
+  return next()
+}
+
+function *genFunc() {
+  const USER_URI = 'https://reqres.in/api/users';
+  let resp = yield axios.get(USER_URI);
+  const userId = resp.data.data[0].id;
+  yield axios.get(`${USER_URI}/${userId}`);
+}
+
+const result = runner(genFunc);
+Promise.resolve(result).then(res => console.log(res.data));
 
 ReactDOM.render(
   <React.StrictMode>
